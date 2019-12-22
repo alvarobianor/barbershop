69a6dc0deaf0cd992022502c340b3f8fb4155400
alvarobianor <bianor.alvaro12@gmail.com>
2019-12-22 17:06:08 -0300

    Models+connection database

--- src/database/migrations/20191222165609-unnamed-migration.js
+++ src/database/migrations/20191222165609-unnamed-migration.js
@@ -1,23 +1,38 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-  },
-
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Users', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+  down: queryInterface => queryInterface.dropTable('users'),
 };
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
@@ -10,5 +11,16 @@ routes.use((req, res, next) => {
 });
 
 routes.get('/opa', (req, res) => res.json({ message: `Axt ${req.name}` }));
+
+routes.post('/', async (req, res) => {
+  const { _name, _email, _password, _provider } = req.body;
+  const user = await User.create({
+    name: _name,
+    email: _email,
+    password_hash: _password,
+    provider: _provider,
+  });
+  return res.json(user);
+});
 
 export default routes;
